6373dc991695820288babea2ae0f8141f5530281
YASSEROVIC <yasserovic@gmail.com>
2023-04-14 03:13:43 -0500

    Uploading the Site
    Upload the site i been working on with all files, images, CSS files ,and JS

---
+++ js/script.js
@@ -0,0 +1,172 @@
+(function($) {
+    "use strict";
+
+    // Windows load
+
+    $(window).on("load", function() {
+
+        // Site loader 
+
+        $(".loader-inner").fadeOut();
+        $(".loader").delay(200).fadeOut("slow");
+
+    });
+
+
+    // Scroll to
+
+    $('a.scroll').smoothScroll({
+        speed: 800,
+        offset: -60
+    });
+
+
+
+    // Site navigation setup
+
+    var header = $('.header'),
+        pos = header.offset(),
+        blockTop = $('.block-top');
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > pos.top + 500 && header.hasClass('default')) {
+            header.fadeOut('fast', function() {
+                $(this).removeClass('default').addClass('switched-header').fadeIn(200);
+                blockTop.addClass('active');
+            });
+        } else if ($(this).scrollTop() <= pos.top + 500 && header.hasClass('switched-header')) {
+            header.fadeOut('fast', function() {
+                $(this).removeClass('switched-header').addClass('default').fadeIn(100);
+                blockTop.removeClass('active');
+            });
+        }
+    });
+
+
+
+
+    //Hero resize
+
+
+    var mainHero = $(" .hero .main-slider .slides li");
+    function mainHeroResize() {
+        mainHero.css('height', $(window).height());
+    }
+
+    $(function() {
+            mainHeroResize()
+        }),
+        $(window).resize(function() {
+            mainHeroResize()
+        });
+
+
+
+
+    // Slider
+
+    $('.main-slider').flexslider({
+        animation: "fade",
+        slideshow: true,
+        directionNav: false,
+        controlNav: true,
+        pauseOnAction: false,
+        animationSpeed: 1000
+    });
+
+
+    $('.review-slider').flexslider({
+        animation: "slide",
+        slideshow: true,
+        directionNav: true,
+        controlNav: false,
+        pauseOnAction: false,
+        animationSpeed: 500
+    });
+
+
+
+
+    // Mobile menu
+
+    var mobileBtn = $('.mobile-but');
+    var nav = $('.main-nav ul.main-menu');
+    var navHeight = nav.height();
+
+    $(mobileBtn).on("click", function() {
+        $(".toggle-mobile-but").toggleClass("active");
+        nav.slideToggle();
+        $('.main-nav li a').addClass('mobile');
+        return false;
+
+
+    });
+
+
+
+    $(window).resize(function() {
+        var w = $(window).width();
+        if (w > 320 && nav.is(':hidden')) {
+            nav.removeAttr('style');
+            $('.main-nav li a').removeClass('mobile');
+        }
+
+    });
+
+
+    // Append images as css background
+
+    $('.background-img').each(function() {
+        var path = $(this).children('img').attr('src');
+        $(this).css('background-image', 'url("' + path + '")').css('background-position', 'initial');
+    });
+
+    // Tabbed content 
+
+    $(".block-tabs li").on("click", function() {
+        if (!$(this).hasClass("active")) {
+            var tabNum = $(this).index();
+            var nthChild = tabNum + 1;
+            $(".block-tabs li.active").removeClass("active");
+            $(this).addClass("active");
+            $(".block-tab li.active").removeClass("active");
+            $(".block-tab li:nth-child(" + nthChild + ")").addClass("active");
+
+        }
+    });
+
+
+   
+
+
+
+    //Popup elements
+
+
+    $('.popup-image').magnificPopup({
+        type: 'image',
+        fixedContentPos: false,
+        fixedBgPos: false,
+        mainClass: 'mfp-no-margins mfp-with-zoom',
+        image: {
+            verticalFit: true
+        },
+        zoom: {
+            enabled: true,
+            duration: 300
+        }
+    });
+
+
+    $('.popup-youtube, .popup-vimeo').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+
+        fixedContentPos: false
+    });
+
+
+})(jQuery);
